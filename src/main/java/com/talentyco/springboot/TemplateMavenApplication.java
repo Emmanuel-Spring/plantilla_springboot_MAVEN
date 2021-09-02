f1a2a70fcfa4291e81a5ec2e6bb8d0eba91a49c5
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2021-09-02 11:07:50 -0400

    maven

---
+++ src/main/java/com/talentyco/springboot/TemplateMavenApplication.java
@@ -0,0 +1,13 @@
+package com.talentyco.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateMavenApplication.class, args);
+	}
+
+}
